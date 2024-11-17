b640da0ae7e8059fb48ff961b3b7c3323607da73
katieyungchung <katieyungchung@g.ucla.edu>
2024-11-17 00:02:05 -0800

    Landing page wip

--- sacks/src/App.js
+++ sacks/src/App.js
@@ -1,29 +1,16 @@
 // import { useEffect, useState } from 'react';
-import useOnLoad from './onLoad';
-import logo from './logo.svg';
 import './App.css';
+import {HeaderDiv, IntroBox, PathDiv} from './home';
+import Footer from './footer';
 
 function App() {
-  const { isLoading, loadDisplay } = useOnLoad(3000);
   return (
-    <div className="Loader">
-      {isLoading ? loadDisplay() :
-      (
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      )}
+    <div className="app-div">
+      <HeaderDiv />
+      {/* <p>---------------------------------</p> */}
+      <IntroBox />
+      <PathDiv></PathDiv>
+      <Footer />
     </div>
   );
 }
---
+++ sacks/src/home.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './home.css'
+import backgroundImage from './assets/plates-background.png';
+import logo from './assets/nomnoms-logo.png';
+import introBackground from './assets/intro-rectangle-background.png';
+import pathBackground from './assets/how-it-works.png';
+
+export function HeaderDiv() {
+    const divStyle = {
+        backgroundImage: `url(${backgroundImage})`, // Set the background image dynamically
+      };
+    
+      return (
+        <div className="header-div" style={divStyle}>
+            <p>Welcome to</p>
+            {/* <h1>NomNoms</h1> */}
+            <img id="logo-title" src={logo} alt="NomNoms logo" />
+            <p>Discover new restaurants near you</p>
+        </div>
+      );
+}
+
+export function IntroBox() {
+    const divStyle = {
+        backgroundImage: `url(${introBackground})`, // Set the background image dynamically
+    };
+    
+      return (
+        <div className="centered-div">
+            <div className="intro-div" style={divStyle}>
+            <div className="body-text">
+                <h2>What is NomNoms?</h2>
+                <h3>We want to create a community where restaurant owners and users can come together and discover new food!</h3>
+            </div>
+            <div className="rectangle"></div>
+            </div>
+        </div>
+      );
+}
+
+export function PathDiv() {
+    const divStyle = {
+        backgroundImage: `url(${pathBackground})`, // Set the background image dynamically
+    };
+    
+      return (
+        <div className="centered-div1">
+            <h1>How it works</h1>
+            <div className="path-div" style={divStyle}>
+            </div> 
+        </div>
+      );
+}
+
