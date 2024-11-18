bef5e89e34fdda76e86481c3f6b1599639a2de98
charlenehuang1 <charlene8688@gmail.com>
2024-11-18 12:23:37 -0800

    login page and navbar

--- sacks/src/App.js
+++ sacks/src/App.js
@@ -1,30 +1,31 @@
 // import { useEffect, useState } from 'react';
 import useOnLoad from './onLoad';
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login.js'
+import Navbar from './components/Navbar.js';
 
 function App() {
   const { isLoading, loadDisplay } = useOnLoad(3000);
+
+
+
   return (
+    <>
+    <Navbar />
     <div className="Loader">
       {isLoading ? loadDisplay() :
       (
         <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Welcome to SACKS Restaurant App! </h1>
+        
+        
       </header>
       )}
+
+      <Login />
     </div>
+    </>
+
   );
 }
 
